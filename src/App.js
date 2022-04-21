9c3207f281de438896d68197691c4c708d9475dd
prakz19 <prakruthiholla1999@gmail.com>
2022-04-21 13:04:13 +0530

    added the login page

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import Peoples from "./components/Peoples";
 import Feedback from "./components/Feedback";
 import Profile from "./components/Profile";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
-        <Header />
         <Routes>
-          <Route path="/" element={<Dashboard />}></Route>
+          <Route path="/" element={<Login />}></Route>
           <Route path="/dashboard" element={<Dashboard />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
           <Route path="/goals" element={<Goals />}></Route>
           <Route path="/feedback" element={<Feedback />}></Route>
           <Route path="/peoples" element={<Peoples />}></Route>
           <Route path="/feedback" element={<Feedback />}></Route>
+          {/* <Footer /> */}
         </Routes>
-        <Footer />
+       
       </Router>
     </>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import"./styles/Login.css"
+
+function Login (){
+
+    return (
+      <div>
+        <h3 className="one">LOGIN PAGE</h3>
+        <form className="box">
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+                
+                <button type="submit" className="btn btn-primary btn-block" >Submit</button>
+                <p className="forgot-password text-right">
+                <a href =""> Forgot password?</a>
+                </p>
+            </form>
+        
+      </div>
+    );
+}
+
+export default Login;
+
+
+
