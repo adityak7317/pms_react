eb7aba751343ebdf251a7d58e7977d83dcc4984a
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2022-05-11 14:40:37 +0530

    did changes in navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -17,72 +17,70 @@ export default function Navbar() {
     <>
       <nav>
         <div className="header">
-          <Link to="/">
-            <img
-              className="logo"
-              src="https://www.ahana.co.in/wp-content/uploads/2019/09/logo-1.png"
-              alt="ahana logo"
-            />
-          </Link>
-          <h3>Performance Management System</h3>
-        </div>
+          {/* logo */}
+          <div className="logo">
+            <Link to="/">
+              <img
+                className="company-logo"
+                src="https://www.ahana.co.in/wp-content/uploads/2019/09/logo-1.png"
+                alt="ahana logo"
+              />
+            </Link>
+          </div>
+          {/* navcontainer */}
+          <div className="nav-container">
+            <div>
+              <span>
+                <h3>Performance Management System</h3>
+              </span>
+            </div>
+            <div className="nav-content">
+              <div>
+                <div className="nav-menu">
+                  <ul>
+                    <li>
+                      <Link to="/Dashboard" className="menu-name">
+                        <RiDashboardFill />
+                        Dashboard
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/Goals" className="menu-name">
+                        <BiTask />
+                        Goals
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/Peoples" className="menu-name">
+                        <MdPeople />
+                        Peoples
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
 
-        <div className="nav-container">
-          <ul>
-            <li className="nav-content">
-              <Link to="/Dashboard" className="menu_name">
-                <RiDashboardFill />
-                Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link to="/Goals" className="menu_name">
-                <BiTask />
-                Goals
-              </Link>
-            </li>
-            <li>
-              <Link to="/Peoples" className="menu_name">
-                <MdPeople />
-                Peoples
-              </Link>
-            </li>
-          </ul>
-          <div className="userdropdown">
-            {/* <div className="user"> */}
-            <button
-              className="dropdown-toggle btn"
-              type="button"
-              data-target="#dropleft"
-              data-toggle="dropdown"
-            >
-              <FaUserTie />
-            </button>
-            {/* <p>Username</p> */}
-            {/* </div> */}
-
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <Link className="dropdown-item" to="/Profile">
-                <ImProfile /> Profile
-              </Link>
-              <Link className="dropdown-item" to="#">
-                <BiUserPin /> Roles
-              </Link>
-              <Link className="dropdown-item" to="/Feedback">
-                <RiFeedbackLine /> Feedback
-              </Link>
-              <Link className="dropdown-item" to="/Login">
-                <FiLogOut /> Logout
-              </Link>
+              <div className="dropdown">
+                <button className="dropbtn">
+                  <FaUserTie />
+                </button>
+                <div className="dropdown-content">
+                  <Link className="dropdown-item" to="/Profile">
+                    <ImProfile /> Profile
+                  </Link>
+                  <Link className="dropdown-item" to="#">
+                    <BiUserPin /> Roles
+                  </Link>
+                  <Link className="dropdown-item" to="/Feedback">
+                    <RiFeedbackLine /> Feedback
+                  </Link>
+                  <Link className="dropdown-item" to="/Login">
+                    <FiLogOut /> Logout
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
-          {/* <li>
-              <Link to="/Profile" className="menu_name">
-                <FaUserTie />
-                Profile
-              </Link>
-            </li> */}
-          {/* </ul> */}
         </div>
       </nav>
     </>
