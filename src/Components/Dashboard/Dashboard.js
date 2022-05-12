0bab43f587f96ccab54319f9b0d8c0f7c70c845a
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2022-05-12 22:34:41 +0530

    modified the data presentation of bargraph

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import BarChart from "./BarChart/BarChart";
-import { UserData } from "./BarChart/Data";
+import { RateData } from "./BarChart/Data";
 // import { Goals } from "./Goals/Goals";
 
 import "./Dashboard.css";
@@ -9,16 +9,29 @@ import Goals from "./D-Goals/D-Goals";
 
 export default function Dashboard() {
   /*data of barchart*/
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
+  const [rateData, setRateData] = useState({
+    labels: RateData.map((data) => data.month),
     datasets: [
       {
-        label: "User Gained",
-        data: UserData.map((data) => data.userGain),
+        label: "Rate Gained",
+        data: RateData.map((data) => data.rate),
         // backgroundColor: ["#fab73fb0"],
       },
     ],
   });
+
+  const style = {
+    bar: {
+      padding: "0%",
+      margin: "0%",
+      display: "flex",
+      flexDirection: "column",
+    },
+    avg: {
+      // margin: "3%",
+      // justifyItems: "center",
+    },
+  };
 
   return (
     <>
@@ -26,9 +39,12 @@ export default function Dashboard() {
         <div className="goals">
           <Goals />
         </div>
-        <div className="Bar">
+        <div className="bar" style={style.bar}>
           <div style={{ width: 700 }}>
-            <BarChart chartData={userData} />
+            <BarChart chartData={rateData} />
+          </div>
+          <div className="average" style={style.avg}>
+            <span>Average Rating : 6.4</span>
           </div>
         </div>
       </div>
