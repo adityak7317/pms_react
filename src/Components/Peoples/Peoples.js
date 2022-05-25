e86e0a6a0e89bde7e5a0bddd5273a23d4d6e1565
Bedasur Veeranna <bedasur.veeranna@ahana.co.in>
2022-05-25 13:03:11 +0530

    people and profile

--- src/Components/Peoples/Peoples.js
+++ src/Components/Peoples/Peoples.js
@@ -1,9 +1,55 @@
-import React from "react";
 
-export default function People() {
-  return (
-    <div>
-      <p>People works</p>
+import {React,useState,useEffect}from"react";
+import axios from"axios";
+import "./Peoles.css"
+import img1 from '../images/profile.jpg'
+
+
+const Peoples=()=>{
+  const [dataList, setDataList]=useState([]);
+  useEffect(()=>{
+    axios.get("http://localhost:5000/api/get").then((response)=>{
+      setDataList(response.data)
+    })
+  },[]);
+   return(
+     
+    <div style={{marginTop:"20px"}}>
+      <h2 className="head" >Team head</h2>
+      <div className="cards">
+                      <div>
+                        <img  className="image" src={img1}/>
+                      </div>
+                    <div className="card-items">
+                        <p>Name: Group Head</p>
+                        <p>Email:sir@gmail.com</p>
+                         <p>Address: asdfasdfasdf</p>
+                     </div>
+                     </div>
+      
+      <h2 className="head" >Team members</h2>
+     
+              {dataList.map((val)=>{
+                return(
+  
+                  
+                     
+                    <div className="cards">
+                      <div>
+                        <img  className="image" src={img1}/>
+                      </div>
+                    <div className="card-items">
+                        <p>{val.e_name}</p>
+                        <p>{val.email}</p>
+                         <p>{val.ph_number}</p>
+                     </div>
+                     </div>
+               
+                      
+                  )
+              })}
+           
     </div>
-  );
-}
+   );
+  } ;
+  export default Peoples;
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,9 +1,63 @@
-import React from "react";
+import {React, useState, useEffect} from 'react'
+import './Profile.css';
+import axios from 'axios';
 
-export default function Profile() {
-  return (
-    <div>
-      <p>Profile works</p>
-    </div>
-  );
-}
+function Profile() {
+  const[infoList, setInfoList]=useState([]);
+  useEffect(()=>{
+    axios.get("http://localhost:5000/api/info").then((response)=>{
+      setInfoList(response.data)
+    })
+  },[]);
+  
+    return (
+        
+      <div>
+       <u> <h5 style={{textAlign:'center',padding:'20px', color:'darkgreen'}}>Employee's Details</h5></u>
+         {infoList.map((val)=>{
+           return(
+            <div className='maindiv'>
+
+              <div className='cardss'>
+                <h4>Basic_details</h4>
+                <hr></hr>
+                {/* <img className='image'  src='https://media.istockphoto.com/vectors/user-avatar-profile-icon-black-vector-illustration-vector-id1209654046?k=20&m=1209654046&s=170667a&w=0&h=BBXhuO36-UfqMS5aYkYvqjAuz3bO1GW-wiXGqRD1Sng='/>
+                <h6>Name:{val.e_name} </h6>
+                <h6>Email:{val.email}  </h6>
+                <h6>Role_id:{val.role_id}</h6>
+                <h6>Dept_id:{val.dept_id}</h6>
+                <h6>E_DOB:cd1-1-1996</h6> */}
+                
+               
+              </div>
+
+                <div className='cardss'>
+                  <h4>Joining_details</h4>
+                  <hr></hr>
+                  {/* <h6>Name:{val.e_name} </h6>
+                <h6>Email:{val.email}  </h6>
+                <h6>Role_id:{val.role_id}</h6>
+                <h6>Dept_id:{val.dept_id}</h6>
+                <h6>E_DOB:cd1-1-1996</h6> */}
+                 
+                </div>
+
+            <div className='cardss'>
+                  <h4>Project_details</h4>
+                  <hr></hr>
+                 
+            </div>
+
+            <div className='cardss'>
+                <h4>Goals_details</h4>
+                <hr></hr>
+               
+            </div>
+
+            </div>
+           )
+         })}
+      </div>    
+    )
+  }
+ export default Profile;
